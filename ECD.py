b97fb528fd6c3b562a3870bf403ae5aa9bf4835d
copper <kingcopper@whu.edu.cn>
2022-06-24 17:52:31 +0800

    remove unused

--- ECD.py
+++ ECD.py
@@ -1,4 +1,7 @@
 import os
+import sys
+import argparse
+sys.path.insert(0,  os.path.join(os.path.dirname(__file__), 'libs'))
 os.environ['PROJ_LIB'] = os.path.join(os.path.dirname(__file__), 'share/proj')
 os.environ['GDAL_DATA'] = os.path.join(os.path.dirname(__file__), 'share')
 os.environ['ECD_BASEDIR'] = os.path.dirname(__file__)
@@ -11,6 +14,6 @@ import logging
 logging.basicConfig(level=logging.INFO, filename=os.path.join(BASE_DIR, 'log.txt'), filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 if __name__ == '__main__':
-
+    
     t = MulStart()
     t.run()    
